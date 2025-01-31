db42a66cf98365fa8a54b065b0a32f7365c4d1c2
Dang Truong <truongminhdang26403@gmail.com>
2025-01-30 23:21:13 -0500

    feat: implement Peterson lock and write unit tests

---
+++ lamp/peterson_lock.h
@@ -0,0 +1,29 @@
+#ifndef PETERSON_LOCK_H_
+#define PETERSON_LOCK_H_
+
+#include "lock.h"
+
+#include <array>
+#include <atomic>
+
+class PetersonLock {
+ public:
+  PetersonLock() {}
+
+  auto Acquire(int id) -> void {
+    int j = 1 - id;
+    flag[id] = true;                    // I'm interested.
+    victim = id;                        // you go first.
+    while (flag[j] && victim == id) {}  // wait
+  }
+
+  auto Release(int id) -> void {
+    flag[id] = false;  // I'm not interested.
+  }
+
+ private:
+  std::array<std::atomic<bool>, 2> flag;
+  std::atomic<int> victim;
+};
+
+#endif  // PETERSON_LOCK_H_
---
+++ tests/peterson_lock_test.cpp
@@ -0,0 +1,81 @@
+#include <gtest/gtest.h>
+#include <atomic>
+#include <thread>
+
+#include "peterson_lock.h"
+
+/**
+ * @brief This test ensures that two threads cannot enter the critical section
+ * simultaneously.
+ */
+TEST(PetersonLockTest, MutualExclusion) {
+  PetersonLock lock;
+  constexpr int num_iterations = 1000;
+  std::atomic<int> counter = 0;
+
+  auto critical_section = [&](int id) {
+    for (int i = 0; i < num_iterations; ++i) {
+      lock.Acquire(id);
+      int expected = counter.load();
+      std::this_thread::yield();  // Yield to encourage race conditions
+      counter.store(expected + 1);
+      EXPECT_EQ(counter.load(), expected + 1);
+      lock.Release(id);
+    }
+  };
+
+  std::thread t1(critical_section, 0);
+  std::thread t2(critical_section, 1);
+
+  t1.join();
+  t2.join();
+}
+
+/**
+ * @brief This test ensures the lock can be acquired and released repeatedly
+ * without issues.
+ */
+TEST(PetersonLockTest, StressTest) {
+  PetersonLock lock;
+  constexpr int num_iterations = 1000000;
+  std::atomic<int> counter = 0;
+
+  auto worker = [&](int id) {
+    for (int i = 0; i < num_iterations; i++) {
+      lock.Acquire(id);
+      counter.fetch_add(1, std::memory_order_relaxed);
+      counter.fetch_sub(1, std::memory_order_relaxed);
+      lock.Release(id);
+    }
+  };
+
+  std::thread t1(worker, 0);
+  std::thread t2(worker, 1);
+
+  t1.join();
+  t2.join();
+
+  EXPECT_EQ(counter.load(), 0);
+}
+
+/**
+ * @brief This test ensures no deadlocks occur.
+ */
+TEST(PetersonLockTest, NoDeadLock) {
+  PetersonLock lock;
+  std::atomic<bool> done = false;
+
+  auto worker = [&](int id) {
+    lock.Acquire(id);
+    done.store(true, std::memory_order_relaxed);
+    lock.Release(id);
+  };
+
+  std::thread t1(worker, 0);
+  std::thread t2(worker, 1);
+
+  t1.join();
+  t2.join();
+
+  EXPECT_TRUE(done.load());
+}
