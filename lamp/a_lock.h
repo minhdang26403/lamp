6ee103f3c7a6a7912ea6cc57e2baba4da3ae5e2b
Dang Truong <truongminhdang26403@gmail.com>
2025-02-09 16:25:25 -0500

    perf: reduce false sharing in ALock by padding each element of the flag array

--- lamp/a_lock.h
+++ lamp/a_lock.h
@@ -6,31 +6,36 @@
 
 #include "lock.h"
 
+struct Flag {
+  bool flag{false};
+  char padding[63];
+};
+
 /**
  * @brief A simple array-based queue lock.
  */
 class ALock : public Lock {
  public:
   ALock(uint64_t capacity) : flag_(capacity), size_(capacity) {
-    flag_[0] = true;
+    flag_[0].flag = true;
   }
 
   auto lock() -> void override {
     uint64_t slot = tail_.fetch_add(1) % size_;
     my_slot_index = slot;
-    while (!flag_[slot]) {}
+    while (!flag_[slot].flag) {}
   }
 
   auto unlock() -> void override {
     uint64_t slot = my_slot_index;
-    flag_[slot] = false;
-    flag_[(slot + 1) % size_] = true;
+    flag_[slot].flag = false;
+    flag_[(slot + 1) % size_].flag = true;
   }
 
   static thread_local uint64_t my_slot_index;
 
  private:
-  std::vector<bool> flag_;
+  std::vector<Flag> flag_;
   std::atomic<uint64_t> tail_{0};
   const uint64_t size_;
 };
