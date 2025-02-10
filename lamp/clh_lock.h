425e86a8ed6fe8c95a904c83f86950ec8648d063
Dang Truong <truongminhdang26403@gmail.com>
2025-02-09 22:20:56 -0500

    feat: implement CLH lock

---
+++ lamp/clh_lock.h
@@ -0,0 +1,54 @@
+#ifndef CLH_LOCK_H_
+#define CLH_LOCK_H_
+
+#include <atomic>
+
+#include "lock.h"
+
+struct QNode {
+  QNode(bool value) : locked(value) {}
+
+  // Record the status of a thread:
+  // - If true, the corresponding thread has either acquired the lock or is
+  // waiting for the lock.
+  // - If false, the thread has released the lock.
+  std::atomic<bool> locked;
+};
+
+class CLHLock : public Lock {
+ public:
+  CLHLock() {
+    auto qnode = new QNode(false);
+    tail_.store(qnode, std::memory_order_relaxed);
+  }
+
+  virtual ~CLHLock() { delete tail_.load(std::memory_order_relaxed); }
+
+  auto lock() -> void override {
+    QNode* qnode = new QNode(true);
+    // Get the node that represents the state of the predecessor thread.
+    QNode* prev = tail_.exchange(qnode, std::memory_order_release);
+
+    // Check if the predecessor thread has acquired the lock or is waiting for
+    // the lock.
+    while (prev->locked.load(std::memory_order_acquire)) {}
+
+    // This thread has acquired the lock and it is now safe to clean up the
+    // memory of the previous node.
+    delete prev;
+
+    // Store my_node for unlock to use.
+    my_node = qnode;
+  }
+
+  auto unlock() -> void override {
+    my_node->locked.store(false, std::memory_order_release);
+  }
+
+ private:
+  QNode* my_node;
+  char padding[64];
+  std::atomic<QNode*> tail_;
+};
+
+#endif  // CLH_LOCK_H_
