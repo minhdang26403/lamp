e8c6cd184942a0f008a98c67bbc2ae6381d952a9
Dang Truong <truongminhdang26403@gmail.com>
2025-02-11 10:34:06 -0500

    fix: make backoff operation thread-safe

--- lamp/backoff.h
+++ lamp/backoff.h
@@ -6,8 +6,8 @@
 #include <thread>
 
 static auto get_random_int64(int64_t upper_limit) -> int64_t {
-  static std::random_device rd;
-  static std::mt19937_64 gen(rd());
+  thread_local std::random_device rd;
+  thread_local std::mt19937_64 gen(rd());  // Each thread gets its own generator
 
   std::uniform_int_distribution<std::int64_t> dist(0, upper_limit);
   return dist(gen);
