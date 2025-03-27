0ae9e4706cb1036839462a162a61280b283dc17b
Dang Truong <truongminhdang26403@gmail.com>
2025-03-26 23:38:30 -0400

    fix: add a new class method to set the stamped pointer and do some small edits

--- lamp/util/atomic_stamped_ptr.h
+++ lamp/util/atomic_stamped_ptr.h
@@ -1,20 +1,23 @@
 #ifndef ATOMIC_STAMPED_PTR_H_
 #define ATOMIC_STAMPED_PTR_H_
 
+#include <gtest/gtest.h>
 #include <atomic>
 
 template<typename T>
 class AtomicStampedPtr {
  public:
+  AtomicStampedPtr() : atomic_stamped_ptr_(StampedPtr{}) {}
+
   AtomicStampedPtr(T* ptr, uint64_t stamp)
-      : atomic_stamped_ptr_(StampedPointer(ptr, stamp)) {}
+      : atomic_stamped_ptr_(StampedPtr(ptr, stamp)) {}
 
   auto compare_and_swap(
       T* expected_ptr, T* desired_ptr, uint64_t expected_stamp,
       uint64_t desired_stamp,
       std::memory_order order = std::memory_order_seq_cst) noexcept -> bool {
-    StampedPointer expected{expected_ptr, expected_stamp};
-    StampedPointer desired{desired_ptr, desired_stamp};
+    StampedPtr expected{expected_ptr, expected_stamp};
+    StampedPtr desired{desired_ptr, desired_stamp};
     return atomic_stamped_ptr_.compare_exchange_strong(expected, desired,
                                                        order);
   }
@@ -23,15 +26,15 @@ class AtomicStampedPtr {
                         uint64_t expected_stamp, uint64_t desired_stamp,
                         std::memory_order success,
                         std::memory_order failure) noexcept -> bool {
-    StampedPointer expected{expected_ptr, expected_stamp};
-    StampedPointer desired{desired_ptr, desired_stamp};
+    StampedPtr expected{expected_ptr, expected_stamp};
+    StampedPtr desired{desired_ptr, desired_stamp};
     return atomic_stamped_ptr_.compare_exchange_strong(expected, desired,
                                                        success, failure);
   }
 
   auto get(std::memory_order order = std::memory_order_seq_cst) const noexcept
       -> std::pair<T*, uint64_t> {
-    StampedPointer stamped_ptr = atomic_stamped_ptr_.load(order);
+    StampedPtr stamped_ptr = atomic_stamped_ptr_.load(order);
     return {stamped_ptr.ptr_, stamped_ptr.stamp_};
   }
 
@@ -45,16 +48,24 @@ class AtomicStampedPtr {
     return atomic_stamped_ptr_.load(order).stamp_;
   }
 
- private:
-  struct StampedPointer {
-    T* ptr_;
-    uint64_t stamp_;
+  auto set(T* ptr, uint64_t stamp,
+           std::memory_order order = std::memory_order_seq_cst) noexcept
+      -> void {
+    StampedPtr stamped_ptr{ptr, stamp};
+    atomic_stamped_ptr_.store(stamped_ptr, order);
+  }
 
-    StampedPointer(T* ptr, uint64_t stamp) noexcept
-        : ptr_(ptr), stamp_(stamp) {}
+ private:
+  struct StampedPtr {
+    T* ptr_{nullptr};
+    uint64_t stamp_{};
+
+    StampedPtr() = default;
+
+    StampedPtr(T* ptr, uint64_t stamp) noexcept : ptr_(ptr), stamp_(stamp) {}
   };
 
-  std::atomic<StampedPointer> atomic_stamped_ptr_;
+  std::atomic<StampedPtr> atomic_stamped_ptr_;
 };
 
 #endif  // ATOMIC_STAMPED_PTR_H_
